c8940cf6a13fc2c30a78fe17790d46845c7e35be
mcclainrt <mcclainrt@gmail.com>
2019-01-23 22:16:12 -0800

    First step

---
+++ catkin_ws/build/apriltags2_ros/CMakeFiles/common.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/common.dir/src/common_functions.cpp.o"
+  "/home/student/thesis/catkin_ws/devel/lib/libcommon.pdb"
+  "/home/student/thesis/catkin_ws/devel/lib/libcommon.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/common.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
