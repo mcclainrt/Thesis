697ebcdb2c4c91b19ebd5b1f2d0154e268aaa4cd
Mcclain, Richard (Rick) (LT) <rtmcclai@nps.edu>
2019-04-24 17:46:28 -0700

    Camera_cal added

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -6,5 +6,13 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
 subdirs(apriltags2_ros/apriltags2)
+subdirs(image_pipeline/image_pipeline)
+subdirs(image_pipeline/camera_calibration)
+subdirs(image_pipeline/image_proc)
+subdirs(image_pipeline/image_publisher)
+subdirs(image_pipeline/image_view)
+subdirs(image_pipeline/stereo_image_proc)
+subdirs(image_pipeline/depth_image_proc)
 subdirs(apriltags2_ros/apriltags2_ros)
+subdirs(image_pipeline/image_rotate)
 subdirs(udpvid)
