90c93fc3c7d188d8b8c7b93f0a6c930c9032ff1b
Rick <35989013+mcclainrt@users.noreply.github.com>
2019-08-24 21:29:48 -0700

    Update Final.m

--- Experiments/Final.m
+++ Experiments/Final.m
@@ -1,5 +1,6 @@
 %% The final product
 % Contains code that is "verified"
+% requires that "Load_all" be ran prior
 
 clear all
 clc
@@ -7,15 +8,15 @@ close all
 
 load('Data/Ex1struct')
 load('Data/Ex2struct')
-load('Data/Ex2structtab')
+load('Data/Ex2structtab') % will remove when I transfer bag files
 load('Data/Ex3struct')
 load('Data/Ex4struct')
 % load('Data/Ex5struct')
 % load('Data/Ex6struct')
 % load('Data/Ex7struct')
 
-Ex2.T = Ex2Tab.T;
-clearvars Ex2Tab
+Ex2.T = Ex2Tab.T;% will remove when I transfer bag files
+clearvars Ex2Tab% will remove when I transfer bag files
 
 Ex2.Results.P = structparse(Ex2.P,5);
 Ex2.Results.V1 = structparse(Ex2.V1,5);
